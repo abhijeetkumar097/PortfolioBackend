59db0cec2c722b0c6348a5ccf34811ab1026cb29
abhijeetkumar097 <aabhijeet097@gmail.com>
2025-06-12 01:37:00 +0530

    updated frontendlink

--- src/main/java/com/example/portfolio/config/SecurityConfig.java
+++ src/main/java/com/example/portfolio/config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("https://portfolio-two-zeta-99.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
